ab0d95c9703b9183372824f9544a689867edfd2a
Eliahae <122315030+Eliahae@users.noreply.github.com>
2023-01-09 21:37:15 +0100

    create index.html file

---
+++ korrektursystem.js
@@ -0,0 +1,20 @@
+/* Funktion zum auswerten des eingabefeldes erster Parameter muss
+ die korrekte eingabe sein die zweite das dazugehörige eingabefeld
+ und das dritte der paragraph indem ausgegeben wird ob die eingabe richtig oder falsch war */
+ function auswerten(loesung, eingabe, ausgabe){
+    var eingabe = document.getElementById(eingabe).value;
+    
+    if (eingabe == loesung){
+    document.getElementById(ausgabe).innerHTML= "Das ist richtig";
+    
+    document.getElementById(eingabe).style.color="green";
+    }
+    else{
+    document.getElementById(ausgabe).innerHTML= "Das ist leider falsch probiere es nochmal";
+    
+    document.getElementById(eingabe).style.border.color="red";
+    }
+    }	
+        
+    
+    
